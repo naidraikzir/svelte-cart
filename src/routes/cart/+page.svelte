ccf274c29a7065ef42251d23a1662920dab485c0
Rizki Ardian <radeite8@gmail.com>
2024-06-15 11:11:25 +0700

    Update to Sveltekit 2

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -5,22 +5,22 @@
 <h2>Cart</h2>
 
 {#each $cart as item}
-	<div style="margin-bottom: 20px;">
-		<div>{item.name}</div>
-		<div>${item.price}</div>
-		<div>
+	<div style="margin-bottom: 10px;">
+		<span>{item.name}</span>
+		<span>${item.price}</span>
+		<span>
 			<button on:click={() => item.amount > 1 && decrement(item)} disabled={item.amount === 1}>
 				-
 			</button>
 			<input type="number" bind:value={item.amount} min="1" />
 			<button on:click={() => increment(item)}>+</button>
 			<button on:click={() => remove(item)}>🗑️</button>
-		</div>
+		</span>
+		<span>${item.amount * item.price}</span>
 	</div>
 {/each}
 
 <div>
 	Total: ${$totalPrice}
+	<button>Buy ({$totalItems})</button>
 </div>
-
-<button>Buy ({$totalItems})</button>
